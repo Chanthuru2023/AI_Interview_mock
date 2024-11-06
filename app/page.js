985d3298486b31142fba43fed9c03e2a1e8c62d5
Chanthuru2023 <146840814+Chanthuru2023@users.noreply.github.com>
2024-11-06 13:07:46 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/dashboard");
+  };
+
   return (
-   <div>
-     <h2> Welcome to MockInterview </h2>
-     <Button>Click</Button>
-   </div>
+    <div>
+      <h2>Welcome to MockInterview</h2>
+      <Button onClick={handleClick}>Click</Button>
+    </div>
   );
 }
